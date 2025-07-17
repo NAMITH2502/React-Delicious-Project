c60a2a8aec31ae226f859f7a7fd78549ff980da9
Your Name <namithpgowda@gmail.com>
2025-07-17 11:36:09 +0530

    full recipe app using reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link } from "react-router-dom";
+import Category from "./components/Category";
+import Pages from "./pages/Pages";
+import Search from "./components/Search";
+import { GiKnifeFork } from "react-icons/gi";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>deliciousss</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 2rem;
+  font-weight: 500;
+  padding-left: 0.5rem;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
